3f1c6bc7829d27dc241b7345c0051b5e171f1b29
foxstacks <198858136+foxstacks@users.noreply.github.com>
2025-05-17 10:28:56 -0500

    forgot to commit

--- script.js
+++ script.js
@@ -3,32 +3,112 @@ const play = document.querySelector('#playbutton')
 const welcome = document.querySelector('.welcome')
 const intro = document.querySelector('.intro')
 const boardcontainer = document.querySelector("#boardcontainer")
+const selectmark = document.querySelector('.selectmark')
+const xselect = document.querySelector('#xlabel')
+const oselect = document.querySelector('#olabel')
+const xselect1 = document.querySelector('#xselect')
+const oselect1 = document.querySelector('#oselect')
+const currentplabel = document.querySelector('#currentplayer')
+const endarea = document.querySelector(".endarea")
+const endofgame = document.querySelector("#endofgame")
+const playagain = document.querySelector("#playagain")
+const winlabel = document.querySelector(".winlabel")
 
 play.addEventListener('click', () => {
     welcome.style.display = 'none'
     play.style.display = 'none'
     intro.style.display = 'none'
-    boardcontainer.style.opacity = '1'
-
+    selectmark.style.opacity = '1'
 })
 
-
 spots.forEach(spot => {
-    spot.addEventListener('click', (e) => {
-        let index = e.target.dataset.index
-        console.log(`You clicked ${index}!`)
+    spot.disabled = true
+});
+
+let playerOneSelect
+let playerTwoSelect
+let playerOneUser
+let playerTwoUser
+let gameControl
+
+xselect.addEventListener('click', () => {
+    xselect.style.display = 'none'
+    oselect.style.display = 'none'
+    playerOneSelect = '×'
+    playerOneUser = 'Player 1'
+    playerTwoSelect = '○'
+    playerTwoUser = 'Player 2'
+    gameControl = playGame(playerOneUser, playerTwoUser, playerOneSelect, playerTwoSelect)
+    currentplabel.style.opacity = '1'
+    boardcontainer.style.opacity = '1'
+    currentplabel.textContent = gameControl.getCurrentPlayerName()
+    spots.forEach(spot => {
+        spot.disabled = false 
+    });
+})
+
+oselect.addEventListener('click', () => {
+    xselect.style.display = 'none'
+    oselect.style.display = 'none'
+    playerOneSelect = '○'
+    playerOneUser = 'Player 1'
+    playerTwoSelect = '×'
+    playerTwoUser = 'Player 2'
+    gameControl = playGame(playerOneUser, playerTwoUser, playerOneSelect, playerTwoSelect)
+    currentplabel.style.opacity = '1'
+    boardcontainer.style.opacity = '1'
+    currentplabel.textContent = gameControl.getCurrentPlayerName()
+    spots.forEach(spot => {
+        spot.disabled = false 
+    });
+})
+
+spots.forEach((spot, index) => {
+    spot.addEventListener('click', () => {
+        if (gameControl) {
+            gameControl.playRound(index)
+            spot.textContent = gameBoard.getBoard()[index]
+            currentplabel.textContent = gameControl.getCurrentPlayerName()
+        }
     })
 });
+
+playagain.addEventListener('click', () => {
+    gameBoard.reset()
+    spots.forEach(spot => {
+        spot.textContent = ''
+        spot.disabled = true
+    })
+    currentplabel.textContent = ''
+    endofgame.style.opacity = ''
+    endarea.style.opacity = ''
+    endofgame.style.pointerEvents = ''
+    xselect.style.display = 'flex'
+    oselect.style.display = 'flex'
+    currentplabel.style.opacity = '0'
+    boardcontainer.style.opacity = '0'
+    winlabel.textContent = ``
+    playerOneSelect = null
+    playerOneUser = null
+    playerTwoSelect = null
+    playerTwoUser = null
+    gameControl = null
+})
 
 const gameBoard = (function () {
     let board = ['', '', '', '', '', '', '', '', '']
     function reset() {
-        board = ['', '', '', '', '', '', '', '', '']
+        for (let i = 0; i < board.length; i++) {
+            board[i] = ''
+        }
     }
     function placeMark(index, marker) {
         board[index] = marker
     }
-    return { board, reset, placeMark }
+    function getBoard() {
+        return board
+    }
+    return { board, reset, placeMark, getBoard }
 })()
 
 function newPlayer(name, marker) {
@@ -43,42 +123,50 @@ function newPlayer(name, marker) {
     return { name, marker, getName, getMarker }
 }
 
+
 const playGame = function (playerOneName, playerTwoName, playerOneMarker, playerTwoMarker) {
     const playerOne = newPlayer(playerOneName, playerOneMarker)
     const playerTwo = newPlayer(playerTwoName, playerTwoMarker)
 
     let currentPlayer;
-    if (playerOneMarker === 'O') {
+    if (playerOneMarker === '○') {
         currentPlayer = playerTwo
     }
     else {
         currentPlayer = playerOne
     }
+
+    function getCurrentPlayerName() {
+        return currentPlayer.name
+    }
+
     function playRound(index) {
         if (gameBoard.board[index] !== '') {
-            console.log("Please choose an empty spot!")
             return
         }
         gameBoard.placeMark(index, currentPlayer.marker)
         console.log(gameBoard.board)
-        checkWin()
-        if (currentPlayer === playerOne) {
-            currentPlayer = playerTwo
-        }
-        else {
-            currentPlayer = playerOne
+        const someoneWon = checkWin()
+        if (!someoneWon) {
+            currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne
         }
 
         function checkWin() {
             const winningCombos = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,5,8], [3,4,5], [6,7,8], [2,4,6]]
-            for (combo of winningCombos) {
+            for (const combo of winningCombos) {
                 const [a, b, c] = combo
                 if (gameBoard.board[a] !== '' && gameBoard.board[a] === gameBoard.board[b] && gameBoard.board[a] === gameBoard.board[c]) {
-                    console.log(`${currentPlayer.name} wins the game!`)
+                console.log(`${currentPlayer.name} has won!`)
+                    spots.forEach(spot => {
+                        spot.disabled = true
+                    });
+                endofgame.style.opacity = '1'
+                endarea.style.opacity = '1'
+                endofgame.style.pointerEvents = 'auto'
+                winlabel.textContent = `${currentPlayer.name} has won!`
                 }
             }
         }
     }    
-
-    return { playRound }
+    return { playRound, getCurrentPlayerName }
 }
